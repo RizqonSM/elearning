699e6e1ca8f32e7128c76a018816ef7524fc4144
akangIko <rizqon.sidik_maulana@yahoo.co.id>
2016-06-12 11:33:03 +0700

    tambah bootstrap dan halaman login

--- application/controllers/cLogin.php
+++ application/controllers/cLogin.php
@@ -1,10 +1,10 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class cLogin extends CI_Controller {
 
-    function __construct() {
-        
-    }
+    public function index()
+	{
+		$this->load->view('vLogin');
+	}
 
 }
-
